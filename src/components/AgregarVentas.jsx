ff445fdbb7121744803f145f58e055a4379fcd8b
Estefac10 <estefac34@gmail.com>
2021-10-22 21:07:00 -0500

    update

--- src/components/AgregarVentas.jsx
+++ src/components/AgregarVentas.jsx
@@ -7,7 +7,7 @@ import Table from "react-bootstrap/Table"
 import swal from 'sweetalert'
 import Swal from "sweetalert2";
 import "../styles/AgregarVentas.css"
-import FormControl from "react-bootstrap/FormControl";
+
 
 
 class AgregarVentas extends Component{
--- src/components/ListProductsService.js
+++ src/components/ListProductsService.js
@@ -1,45 +1,6 @@
 import axios from "axios";
 import { Path } from "../config";
 
-let Productos = [
-  {
-    id: 0,
-    nombre: "Cuadernos",
-    precio: 2000,
-    imagen:
-      "https://st.depositphotos.com/1875497/3781/i/600/depositphotos_37810929-stock-photo-books-on-white.jpg",
-    cantidad: 10,
-    descripcion: "descripcion del producto",
-  },
-  {
-    id: 1,
-    nombre: "Colores",
-    precio: 5000,
-    imagen:
-      "https://panamericana.vteximg.com.br/arquivos/ids/343890-600-690/ecolapices-faber-castell-supersoft-por-12-unidades-7891360662471.jpg?v=637135805926930000",
-    cantidad: 10,
-    descripcion: "descripcion del producto",
-  },
-  {
-    id: 2,
-    nombre: "Resma",
-    precio: 10000,
-    imagen:
-      "https://medios.papeleriamodelo.com/wp-content/uploads/2015/06/resma-carta-reprograf.jpg",
-    cantidad: 10,
-    descripcion: "descripcion del producto",
-  },
-  {
-    id: 3,
-    nombre: "Borrador",
-    precio: 1000,
-    imagen:
-      "https://distribuidorasurtitodo.com.co/wp-content/uploads/2020/08/borrador-pelikan-pz-20-grande-unidad.jpg",
-    cantidad: 10,
-    descripcion: "descripcion del producto",
-  },
-];
-
 export function getProducts() {
   return axios.get(`${Path.url}/products`);
 }
